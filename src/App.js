1df982b78b5cb82f6975155c17e7c4eeba774cdc
Touza2el <59705964+Touza2el@users.noreply.github.com>
2020-11-10 21:16:08 +0100

    nn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 import './App.scss';
 
 function App() {
+  const [users, setUsers] = useState([
+    { id: 1, name: 'Hassan' },
+    { id: 2, name: 'Mustapha' },
+    { id: 3, name: 'Younes' },
+  ]);
+  // useEffect(() => {
+  //   try {
+  //     const fetchData = async () => {
+  //       const { data } = await Axios.get();
+  //       setUsers(data);
+  //     };
+  //     fetchData();
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // });
   return (
     <div className='app'>
-      <h1>Web / Mobile Developer</h1>
+      <h1>Web / Mobile Developers</h1>
+      <div>
+        {users.map((user) => (
+          <div key={user.id}>{user.name}</div>
+        ))}
+      </div>
     </div>
   );
 }
