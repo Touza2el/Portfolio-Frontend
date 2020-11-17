73e7f5a4ee4c563b3e25e00ad8ce31ed41057459
Touza2el <59705964+Touza2el@users.noreply.github.com>
2020-11-17 17:33:19 +0100

    adding Hero Image

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,29 +3,36 @@ import { Link } from 'react-router-dom';
 const HeroSection = () => {
   return (
     <section className='hero-section'>
-      <div className='hero-section-content'>
-        <div className='hero-icon'>
-          <Link
-            to={{
-              pathname: 'https://github.com/Touza2el',
-            }}
-            target='_blank'
-          >
-            <i className='fab fa-github'></i>
-          </Link>
-        </div>
-        <div className='hero-title'>
-          <h1>Front-End Developer</h1>
-        </div>
-        <div className='hero-description'>
-          <p>
-            I'm Front-End web developer, I build fast, modern and mobile
-            friendly websites and web applications using modern web technologies
-            : HTML, CSS, Javascript and React.
-          </p>
-        </div>
-        <div className='hero-btn'>
-          <Link to='/contact'>CONTACT ME</Link>
+      <div className='container'>
+        <div className='hero-section-content'>
+          <div className='hero-section-text'>
+            <div className='hero-icon'>
+              <Link
+                to={{
+                  pathname: 'https://github.com/Touza2el',
+                }}
+                target='_blank'
+              >
+                <i className='fab fa-github'></i>
+              </Link>
+            </div>
+            <div className='hero-title'>
+              <h1>Front-End Developer</h1>
+            </div>
+            <div className='hero-description'>
+              <p>
+                I'm Front-End web developer, I build fast, modern and mobile
+                friendly websites and web applications using modern web
+                technologies : HTML, CSS, Javascript and React.
+              </p>
+            </div>
+            <div className='hero-btn'>
+              <Link to='/contact'>CONTACT ME</Link>
+            </div>
+          </div>
+          <div className='hero-section-image'>
+            <img src='/images/hero.png' alt='desk' />
+          </div>
         </div>
       </div>
     </section>
