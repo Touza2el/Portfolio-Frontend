e2742fcd5f97a99f4368d890a6545280f5ca0933
Touza2el <59705964+Touza2el@users.noreply.github.com>
2020-11-19 17:40:32 +0100

    hhhh

--- src/data.js
+++ src/data.js
@@ -5,42 +5,42 @@ export const data = {
       icon: 'far fa-window-restore',
       title: 'Web Design',
       description:
-        'Design user friendely web and mobile applications, using modern tools and technologies.',
+        'Wireframing, designing and prototyping user friendely web and mobile applications.',
     },
     {
       id: 2,
       icon: 'fas fa-laptop-code',
       title: 'Front-End Development',
       description:
-        '  Create fast, modern & mobile friendly website and web applications using modern web technologies.',
+        '  Create fast, modern & mobile friendly website and web applications using latest web technologies.',
     },
     {
       id: 3,
       icon: 'fas fa-cogs',
       title: 'Back-End Development',
       description:
-        ' Building Rest APIs and Managing the backend of a website, interacting with Databases.',
+        ' Creating API Endpoints, Managing the backend of a website, and interacting with Databases.',
     },
     {
       id: 4,
       icon: 'fab fa-wordpress-simple',
       title: 'Wordpress Design',
       description:
-        ' Set up a WordPress website and setting configuration, using pre-made themes and plugins.',
+        ' Set up WordPress, configuration, and design website using pre-made themes and plugins.',
     },
     {
       id: 5,
       icon: 'fas fa-search-dollar',
       title: 'On-Page SEO',
       description:
-        'Meta tags, meta description and meta keywords, optimizing images and add sitemap for search engines.',
+        'Meta tags, meta description, meta keywords and optimizing images for search engines.',
     },
     {
       id: 6,
       icon: 'fas fa-cloud-upload-alt',
       title: 'Deployment',
       description:
-        'Domain Names, web hosting and SSL configuration, upload website and web applications to the internet.',
+        'Domain Name, web hosting and SSL configuration, and uploading website to the internet.',
     },
   ],
 };
